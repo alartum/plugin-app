fe8ed8e7d474b04ded06c198025fa62192974f88
Aleksandr Artemenkov <alartum@gmail.com>
2017-06-03 01:37:25 +0300

    Fixed .gitignore

--- printer/helloprinter.cpp
+++ printer/helloprinter.cpp
@@ -5,21 +5,19 @@ void HelloPrinter::print()
     std::cout << "Hello, World!" << std::endl;
 }
 
-extern "C" {
-    Printer* constructor(){
-       return new HelloPrinter;
-    }
-
-    class proxy {
-    public:
-       proxy(){
-          // register the maker with the factory
-          //constructors["HelloPrinter"] = constructor;
-          std::cout << "a = " << a << std::endl;
-          std::cout << "proxy()" << std::endl;
-       }
-    };
-    // our one instance of the proxy
-    proxy p;
+Printer* constructor(){
+   return new HelloPrinter;
 }
+
+class proxy {
+public:
+   proxy(){
+      // register the maker with the factory
+      //constructors["HelloPrinter"] = constructor;
+      std::cout << "a = " << a << std::endl;
+      std::cout << "proxy()" << std::endl;
+   }
+};
+// our one instance of the proxy
+proxy p;
 
